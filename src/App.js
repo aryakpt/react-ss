f43f2a4e4131ac5b06b87d050684e6705ccec49e
aryakpt <aryakrisna07@gmail.com>
2022-09-02 14:59:40 +0700

    task 5 - react side effect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { Section2, Footer } from "./components";
 
--- src/components/Section2/index.js
+++ src/components/Section2/index.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Section2.css";
+
 function Section2({ fullName }) {
   const [name, setName] = useState(fullName);
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `${count} ${name}`;
+  }, [name, count]);
 
   const onClickHandler = () => {
     setName("New Name");
